c30c715a40ddd938d94c844dec23ccd289f71518
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-05 18:02:10 -0500

    Added Routers
    In routes folder, touched clients, donation, donor, initiative routers and updated AppRouter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,3 +1,13 @@
-const Router = require('express').Router()
+const Router = require('express').Router();
+const DonorRouter = require('./DonorRouter');
+const DonationRouter = require('./DonationRouter');
+const ClientsRouter = require('./ClientsRouter');
+const InitiativeRouter = require('./InitiativeRouter');
 
-module.exports = Router
+Router.get('/', (req, res) => res.json({ message: 'API Router Works' }));
+Router.use('/donors', DonorRouter);
+Router.use('/donation', DonationRouter);
+Router.use('/client', ClientsRouter);
+Router.use('/initiative', InitiativeRouter);
+
+module.exports = Router;
