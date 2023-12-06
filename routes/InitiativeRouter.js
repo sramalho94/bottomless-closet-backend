52b619eec228acb0e260c1ee3f07007168143bf1
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-05 19:19:03 -0500

    Comment out code that is not ready to be implemented

--- routes/InitiativeRouter.js
+++ routes/InitiativeRouter.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const InitiativeController = require('../controllers/InitiativeController');
+const router = require('express').Router()
+// const InitiativeController = require('../controllers/InitiativeController')
 
-router.post('/initiative', InitiativeController.create);
-router.get('/initiative', InitiativeController.getAll);
-router.get('/initiative/:id', InitiativeController.getOne);
-router.put('/initiative/:id', InitiativeController.update);
-router.delete('/initiative/:id', InitiativeController.delete);
+// router.post('/initiative', InitiativeController.create);
+// router.get('/initiative', InitiativeController.getAll);
+// router.get('/initiative/:id', InitiativeController.getOne);
+// router.put('/initiative/:id', InitiativeController.update);
+// router.delete('/initiative/:id', InitiativeController.delete);
 
-module.exports = router;
+module.exports = router
