7faac4233646c330a70e1739a925acdf8467736b
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-06 17:08:54 -0500

    Add config for prod

--- config/config.js
+++ config/config.js
@@ -16,20 +16,19 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432
+  },
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-  // TODO: come back here where deploying
-  // production: {
-  //   username: process.env.DB_USERNAME_PROD,
-  //   password: process.env.DB_PASSWORD_PROD,
-  //   database: process.env.DB_NAME_PROD,
-  //   host: process.env.DB_HOST_PROD,
-  //   port: process.env.DB_PORT || 5432,
-  //   dialect: 'postgres',
-  //   dialectOptions: {
-  //     ssl: {
-  //       require: true,
-  //       rejectUnauthorized: false
-  //     }
-  //   }
-  // }
 }
