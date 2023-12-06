22417d45b4f6ccbfcc66dbed22eb7daa09d6586e
noordibou <noor.dibou@hotmail.com>
2023-12-05 19:45:58 -0600

    added client controllers and cleaned up donor

---
+++ controllers/ClientController.js
@@ -0,0 +1,47 @@
+const { Client } = require("../models");
+
+class ClientController {
+  async createClient(req, res) {
+    try {
+      const client = await Client.create(req.body);
+      res.status(201).json({ client });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+
+  async deleteClient(req, res) {
+    try {
+      const { id } = req.params;
+      const deleted = await Client.destroy({
+        where: { id: id },
+      });
+      if (deleted) {
+        return res.status(204).send("Client deleted");
+      }
+      throw new Error("Client not found");
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+  async getAllClients(req, res) {
+    try {
+      const clients = await Client.findAll();
+      res.status(200).json(clients);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  }
+
+  async getClientById(req, res) {
+    try {
+      const client = await Client.findByPk(req.params.id);
+      res.status(200).json(client);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  }
+}
+
+module.exports = new ClientController();
--- routes/DonorRouter.js
+++ routes/DonorRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const DonorController = require('../controllers/DonorController')
 
-router.post('/donor', DonorController.createDoner)
-router.get('/donor', DonorController.getAllDoners)
-router.get('/donor/:id', DonorController.getDonerById)
-router.delete('/donor/:id', DonorController.deleteDoner)
+router.post('/donor', DonorController.createDonor)
+router.get('/donor', DonorController.getAllDonors)
+router.get('/donor/:id', DonorController.getDonorById)
+router.delete('/donor/:id', DonorController.deleteDonor)
 
 module.exports = router
